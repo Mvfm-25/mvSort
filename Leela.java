a0421982356c5afd9d8bc1b998442432efcd4710
Mvfm <mvfm.filho@hotmail.com>
2026-01-09 14:16:48 -0300

    Implementação da função de teste de sort. Mais modular!

--- Leela.java
+++ Leela.java
@@ -1,7 +1,7 @@
 // Classe principal para testar cada um dos sorts implementados.
 // [mvfm]
 //
- // Criado : 07/01/2026  || Última modificação : 07/01/2026
+ // Criado : 07/01/2026  || Última modificação : 09/01/2026
 
 import java.util.Arrays;
 
@@ -11,15 +11,29 @@ public class Leela {
         // Chamando sort.
         Sort sorter = new BubbleSort();
 
-        Integer[] lista1 = {7, 4, 3, 2, 1, 6, 8, 5, 9, 10};
-        // Re-uso temporário, ainda não quero retornar novas listas.
-        Integer[] lista2 = {7, 4, 3, 2, 1, 6, 8, 5, 9, 10};
-        System.out.println("Lista original : " + Arrays.toString(lista1));
+        Integer[] lista = {7, 4, 3, 2, 1, 6, 8, 5, 9, 10};
+        System.out.println();
 
-        System.out.println("Selection sort : " + Arrays.toString(sorter.sort(lista1)));
+        // Agora fica mais modular!
+        testaSort(new SelectionSort(), lista, "Selection Sort");
+        testaSort(new BubbleSort(), lista, "Bubble Sort");
+    }
 
-        sorter = new BubbleSort();
-        System.out.println("Buble sort : " + Arrays.toString(sorter.sort(lista2)));
+    // Função para facilitação de testes de cada uma das implementações.
+    public static void testaSort(Sort alg, Integer[] lista, String nomeAlg){
+        // Justamente pra não ter que criar uma nova lista para cada teste.
+        Integer[] copia = Arrays.copyOf(lista, lista.length);
+
+        // Medindo tempo, só por curiosidade!
+        // Procurar implementar cronometragem de 'complexidade' depois.
+        long inicio = System.nanoTime();
+        Integer[] listaOrdenada = alg.sort(copia);
+        long fim = System.nanoTime();
+
+        // Prints informativos.
+        System.out.println(nomeAlg + " :" + Arrays.toString(listaOrdenada));
+        System.out.println("Tempo : " + (fim - inicio) / 1_000_000.0 + " ms");
+        System.out.println();
     }
 
 }
--- QuickSort.java
+++ QuickSort.java
@@ -7,7 +7,6 @@
 public class QuickSort implements Sort{
 
     public Integer[] sort(Integer[] lista){
-
         return lista;
     }
 
